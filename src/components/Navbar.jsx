6f7757fd2f68b973ea428ad525bcc8c8201e9c27
adamowski21 <76078599+adamowski21@users.noreply.github.com>
2024-02-27 23:43:48 +0100

    Navbar completed.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+
+    return (
+        <div>
+            <nav className='justify-around items-center h-17vh lg:flex hidden'>
+                <div className="text-3xl cursor-default">
+                    Adam Michałowski
+                </div>
+                <div>
+                    <ul className="flex gap-8 list-none text-2xl">
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#experience">Experience</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <nav className="flex justify-around items-center h-17vh lg:hidden">
+                <div className="text-3xl cursor-default">
+                    Adam Michałowski
+                </div>
+                <div className="relative inline-block">
+                    <div className="flex flex-col justify-between h-6 w-8 cursor-pointer" onClick={toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    {showMenu && (
+                        <div className="absolute top-full right-0 bg-white w-fit transition-all duration-300 ease-in-out">
+                            <ul className="flex flex-col gap-8 list-none text-2xl p-2">
+                                <li><a className="block p-1 text-center text-2xl text-black no-underline transition-all duration-300 ease-in-out" href="#about" onClick={toggleMenu}>About</a></li>
+                                <li><a className="block p-1 text-center text-2xl text-black no-underline transition-all duration-300 ease-in-out" href="#experience" onClick={toggleMenu}>Experience</a></li>
+                                <li><a className="block p-1 text-center text-2xl text-black no-underline transition-all duration-300 ease-in-out" href="#projects" onClick={toggleMenu}>Projects</a></li>
+                                <li><a className="block p-1 text-center text-2xl text-black no-underline transition-all duration-300 ease-in-out" href="#contact" onClick={toggleMenu}>Contact</a></li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </div>
+
+    )
+}
+
+export default Navbar
